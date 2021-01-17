0752a41f038bc3317bea8072b10a4ff4fefa9671
sorashin <naka52shin@gmail.com>
2021-01-18 03:22:39 +0900

    init

---
+++ build/precache-manifest.9e4ecb922ae73e8e836071bd836dfd07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cead843ca943b50754b25c60691b7b2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80189d84bfbcacc8d053",
+    "url": "/static/css/main.46a5a087.chunk.css"
+  },
+  {
+    "revision": "40b90729bccfa1da140a",
+    "url": "/static/js/2.bdcf35e5.chunk.js"
+  },
+  {
+    "revision": "80189d84bfbcacc8d053",
+    "url": "/static/js/main.43b9e05d.chunk.js"
+  },
+  {
+    "revision": "7107b225f6f8f9831472",
+    "url": "/static/js/runtime-main.08150bc2.js"
+  }
+]);
